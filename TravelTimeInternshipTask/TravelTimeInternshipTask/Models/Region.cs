838cdb6211827db7828d50d08008ee58d5370363
nesterxdd <nesterknight7@gmail.com>
2025-07-08 14:03:14 +0300

    comments are removed

--- TravelTimeInternshipTask/TravelTimeInternshipTask/Models/Region.cs
+++ TravelTimeInternshipTask/TravelTimeInternshipTask/Models/Region.cs
@@ -36,34 +36,6 @@ namespace TravelTimeInternshipTask.Models
             }
         }
 
-        //old method
-        //public bool IsInRegion(Location location)
-        //{
-        //    for (int i = 0; i < Coordinates.Count; i++)
-        //    {
-        //        int intersectCount = 0;
-        //        for (int j = 0; j < Coordinates[i].Count ; j++)
-        //        {
-        //            List<double> c1 = Coordinates[i][j];
-        //            List<double> c2 = Coordinates[i][(j+1) % Coordinates[i].Count];
-
-        //            bool intersect = (c1[1] > location.Coordinates[1]) != (c2[1] > location.Coordinates[1]) && (location.Coordinates[0] < (c2[0] - c1[0]) * (location.Coordinates[1] - c1[1]) / (c2[1] - c1[1] + 0.00000000000000000001) + c1[0]);
-        //            //bool intersect = (c1.Y > location.Coordinates.Y) != (c2.Y > location.Coordinates.Y) && (location.Coordinates.X < (c2.X - c1.X) * (location.Coordinates.Y - c1.Y) / (c2.Y - c1.Y + 0.1m) + c1.X);
-
-        //            if (intersect)
-        //            {
-        //                intersectCount++;
-        //            }
-
-        //        }
-        //        if (intersectCount % 2 != 0)
-        //        {
-        //            return true;
-        //        }
-        //    }
-        //    return false;
-        //}
-
         /// <summary>
         /// Checks if the location is inside of the region (inside one of the polygon of region)
         /// </summary>
